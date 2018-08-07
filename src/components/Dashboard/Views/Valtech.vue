02d94c2731e462561086dcc1610e0f5691d669e5
Thiago Pereira <thiagopereira2634@gmail.com>
2018-08-06 21:59:11 -0300

    update - update doublequote to singlequote

--- src/components/Dashboard/Views/Valtech.vue
+++ src/components/Dashboard/Views/Valtech.vue
@@ -57,77 +57,77 @@
 }
 </style>
 <script>
-import Card from "@/components/UIComponents/Card.vue";
+import Card from '@/components/UIComponents/Card.vue';
 export default {
     components: {
         Card
     },
     data() {
         return {
-        cards: []
+            cards: []
         };
     },
     created() {
         this.cards = this.getCards();
     },
     methods: {
-        getCards: function() {
+        getCards: function () {
             return [
                 {
-                    title: "Valtech_",
-                    subtitle: "Front-end",
+                    title: 'Valtech_',
+                    subtitle: 'Front-end',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "__full",
-                    imageUrl: "Lagoa.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '__full',
+                    imageUrl: 'Lagoa.jpg'
                 },
                 {
-                    title: "Venice",
-                    subtitle: "Italy",
+                    title: 'Venice',
+                    subtitle: 'Italy',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "__first-half",
-                    imageUrl: "Venice.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '__first-half',
+                    imageUrl: 'Venice.jpg'
                 },
                 {
-                    title: "Berlin",
-                    subtitle: "Germany",
+                    title: 'Berlin',
+                    subtitle: 'Germany',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "",
-                    imageUrl: "Berlin.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '',
+                    imageUrl: 'Berlin.jpg'
                 },
                 {
-                    title: "Barcelona",
-                    subtitle: "Spain",
+                    title: 'Barcelona',
+                    subtitle: 'Spain',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "",
-                    imageUrl: "Barcelona.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '',
+                    imageUrl: 'Barcelona.jpg'
                 },
                 {
-                    title: "Paris",
-                    subtitle: "France",
+                    title: 'Paris',
+                    subtitle: 'France',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "",
-                    imageUrl: "Paris.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '',
+                    imageUrl: 'Paris.jpg'
                 },
                 {
-                    title: "Amsterdam",
-                    subtitle: "Netherlands",
+                    title: 'Amsterdam',
+                    subtitle: 'Netherlands',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "",
-                    imageUrl: "Amsterdam.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '',
+                    imageUrl: 'Amsterdam.jpg'
                 },
                 {
-                    title: "London",
-                    subtitle: "United Kindom",
+                    title: 'London',
+                    subtitle: 'United Kindom',
                     text:
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.",
-                    class: "__second-half",
-                    imageUrl: "London.jpg"
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu iaculis libero.',
+                    class: '__second-half',
+                    imageUrl: 'London.jpg'
                 }
             ];
         }
